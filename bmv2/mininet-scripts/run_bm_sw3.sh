031735de0758246bffeac7a814ecdfe376db367d
Shridevi Gurram <shridevi.gurram@gmail.com>
2016-05-22 22:31:15 -0700

    fixed the switch info

--- bmv2/mininet-scripts/run_bm_sw3.sh
+++ bmv2/mininet-scripts/run_bm_sw3.sh
@@ -1,1 +1,1 @@
-sudo ./simple_switch --log-console  -i 1@sw1-eth1 -i 2@sw1-eth2 -i 3@sw1-eth3 -i 4@sw1-eth4 -i 64@veth250 --thrift-port 10001 --pcap $*  ../share/bmpd/switch/switch.json
+sudo ./simple_switch --log-console  -i 1@sw3-eth1 -i 2@sw3-eth2 -i 3@sw3-eth3 -i 4@sw3-eth4 -i 64@veth250 --thrift-port 10001 --pcap $*  ../share/bmpd/switch/switch.json
--- bmv2/mininet-scripts/run_bm_sw4.sh
+++ bmv2/mininet-scripts/run_bm_sw4.sh
@@ -1,1 +1,1 @@
-sudo ./simple_switch --log-console  -i 1@sw1-eth1 -i 2@sw1-eth2 -i 3@sw1-eth3 -i 4@sw1-eth4 -i 64@veth250 --thrift-port 10001 --pcap $*  ../share/bmpd/switch/switch.json
+sudo ./simple_switch --log-console  -i 1@sw4-eth1 -i 2@sw4-eth2 -i 3@sw4-eth3 -i 4@sw4-eth4 -i 64@veth250 --thrift-port 10001 --pcap $*  ../share/bmpd/switch/switch.json
